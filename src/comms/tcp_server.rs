0e42b5a6661f7fe71d167434a40347fa1a2576f4
ElBellaCiao <aguilarvtrading@gmail.com>
2025-07-26 21:45:56 +0000

    use T as type for buffer

--- src/comms/tcp_server.rs
+++ src/comms/tcp_server.rs
@@ -3,13 +3,13 @@ use bytemuck::Pod;
 use std::io::{BufReader, Read};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 
-pub struct TcpServer<const BUFFER_SIZE: usize> {
+pub struct TcpServer<T: Pod> {
     pub listener: TcpListener,
     stream: BufReader<TcpStream>,
-    buffer: [u8; BUFFER_SIZE],
+    buffer: [T; 1],
 }
 
-impl<const BUFFER_SIZE: usize> TcpServer<BUFFER_SIZE> {
+impl<T: Pod> TcpServer<T> {
     pub fn new(socket_addr: SocketAddr) -> Result<Self> {
         let listener = TcpListener::bind(socket_addr)?;
         let (stream, _addr) = listener.accept()?;
@@ -20,15 +20,14 @@ impl<const BUFFER_SIZE: usize> TcpServer<BUFFER_SIZE> {
         Ok(Self {
             listener,
             stream: BufReader::new(stream),
-            buffer: [0; BUFFER_SIZE],
+            buffer: [T::zeroed(); 1],
         })
     }
 
     #[inline(always)]
-    pub fn receive<T: Pod>(&mut self) -> Result<T> {
-        const { assert!(size_of::<T>() == BUFFER_SIZE) };
-
-        self.stream.read_exact(&mut self.buffer)?;
-        Ok(*bytemuck::from_bytes(&self.buffer))
+    pub fn receive(&mut self) -> Result<T> {
+        let bytes = bytemuck::bytes_of_mut(&mut self.buffer[0]);
+        self.stream.read_exact(bytes)?;
+        Ok(self.buffer[0])
     }
 }
