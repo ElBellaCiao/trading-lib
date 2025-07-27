296ee6caf358c43727a37f8cf34d5c1e23d8db50
ElBellaCiao <aguilarvtrading@gmail.com>
2025-07-27 05:30:59 +0000

    refactor tcp client

--- src/comms/tcp_client.rs
+++ src/comms/tcp_client.rs
@@ -7,11 +7,10 @@ use std::collections::HashMap;
 use std::io::Write;
 use std::net::IpAddr;
 use std::net::TcpStream;
-use std::time::Duration;
 
 pub struct TcpClient<const MAX_IPS: usize, const MAX_IPS_PER_INSTRUMENT: usize> {
     instrument_to_ips:
-        FnvIndexMap<InstrumentId, heapless::Vec<TcpStream, MAX_IPS_PER_INSTRUMENT>, MAX_IPS>,
+        FnvIndexMap<InstrumentId, TcpConnectionPool<MAX_IPS_PER_INSTRUMENT>, MAX_IPS>,
 }
 
 impl<const MAX_IPS: usize, const MAX_IPS_PER_INSTRUMENT: usize>
@@ -25,26 +24,18 @@ impl<const MAX_IPS: usize, const MAX_IPS_PER_INSTRUMENT: usize>
 
     #[inline(always)]
     pub fn send<T: Pod>(&mut self, instrument_id: &InstrumentId, data: &T) -> Result<()> {
-        let ips = self
+        let connection_pool = self
             .instrument_to_ips
             .get_mut(instrument_id)
             .ok_or_else(|| anyhow!("Instrument {} not found", instrument_id))?;
 
-        let bytes = bytemuck::bytes_of(data);
-
-        for stream in ips.iter_mut() {
-            stream.write_all(bytes)?;
-            stream.flush()?;
-        }
-
-        Ok(())
+        connection_pool.send_to_all(data)
     }
 
     fn create_fixed_map(
         address_book: HashMap<InstrumentId, Vec<IpAddr>>,
         port: u16,
-    ) -> Result<FnvIndexMap<InstrumentId, heapless::Vec<TcpStream, MAX_IPS_PER_INSTRUMENT>, MAX_IPS>>
-    {
+    ) -> Result<FnvIndexMap<InstrumentId, TcpConnectionPool<MAX_IPS_PER_INSTRUMENT>, MAX_IPS>> {
         let mut fixed_map = FnvIndexMap::new();
         for (instrument_id, ips) in address_book {
             if ips.len() > MAX_IPS_PER_INSTRUMENT {
@@ -56,23 +47,50 @@ impl<const MAX_IPS: usize, const MAX_IPS_PER_INSTRUMENT: usize>
                 );
             }
 
-            let mut ip_list = heapless::Vec::new();
+            let mut connection_pool = TcpConnectionPool::new();
             for ip in ips {
-                let stream =
-                    TcpStream::connect_timeout(&(ip, port).into(), Duration::from_secs(20))
-                        .map_err(|e| anyhow::anyhow!("Failed to connect to {}: {}", ip, e))?;
-                stream.set_nodelay(true)?;
-                stream.set_nonblocking(false)?;
-
-                ip_list.push(stream).map_err(|_| {
-                    anyhow::anyhow!("Failed to push IP for instrument {}", instrument_id)
-                })?;
+                connection_pool.connect_and_add(ip, port)?;
             }
+
             fixed_map
-                .insert(instrument_id, ip_list)
+                .insert(instrument_id, connection_pool)
                 .map_err(|_| anyhow::anyhow!("Too many instruments: maximum is {}", MAX_IPS))?;
         }
-
         Ok(fixed_map)
+    }
+}
+
+pub struct TcpConnectionPool<const MAX_CONNECTIONS: usize> {
+    streams: heapless::Vec<TcpStream, MAX_CONNECTIONS>,
+}
+
+impl<const MAX_CONNECTIONS: usize> TcpConnectionPool<MAX_CONNECTIONS> {
+    fn new() -> Self {
+        Self {
+            streams: heapless::Vec::new(),
+        }
+    }
+
+    fn connect_and_add(&mut self, ip: IpAddr, port: u16) -> Result<()> {
+        let stream = TcpStream::connect((ip, port))
+            .map_err(|e| anyhow::anyhow!("Failed to connect to {}: {}", ip, e))?;
+        stream.set_nodelay(true)?;
+        stream.set_nonblocking(false)?;
+
+        self.streams
+            .push(stream)
+            .map_err(|_| anyhow::anyhow!("Too many connections: maximum is {}", MAX_CONNECTIONS))
+    }
+
+    #[inline(always)]
+    pub fn send_to_all<T: Pod>(&mut self, data: &T) -> Result<()> {
+        let bytes = bytemuck::bytes_of(data);
+
+        for stream in &mut self.streams {
+            stream.write_all(bytes)?;
+            stream.flush()?;
+        }
+
+        Ok(())
     }
 }
