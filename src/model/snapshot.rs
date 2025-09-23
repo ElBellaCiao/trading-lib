62dd512a3ed830257f4b04a1dd31d94b3b5bb016
ElBellaCiao <aguilarvtrading@gmail.com>
2025-09-23 03:48:59 +0000

    export book depth constant

--- src/model/snapshot.rs
+++ src/model/snapshot.rs
@@ -3,16 +3,18 @@ use bytemuck::{Pod, Zeroable};
 
 type Quantity = u32;
 
+pub const BOOK_DEPTH: usize = 10;
+
 #[repr(C, align(64))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Pod, Zeroable)]
 pub struct BookSnapshot {
     pub timestamp: Timestamp,
-    pub best_bid_price: [Price; 10],
-    pub best_bid_volume: [Volume; 10],
-    pub best_bid_quantity: [Quantity; 10],
-    pub best_ask_price: [Price; 10],
-    pub best_ask_volume: [Volume; 10],
-    pub best_ask_quantity: [Quantity; 10],
+    pub best_bid_price: [Price; BOOK_DEPTH],
+    pub best_bid_volume: [Volume; BOOK_DEPTH],
+    pub best_bid_quantity: [Quantity; BOOK_DEPTH],
+    pub best_ask_price: [Price; BOOK_DEPTH],
+    pub best_ask_volume: [Volume; BOOK_DEPTH],
+    pub best_ask_quantity: [Quantity; BOOK_DEPTH],
     pub _padding_0: [u8; 32],
     pub _padding_1: [u8; 16],
     pub _padding_2: [u8; 8],
